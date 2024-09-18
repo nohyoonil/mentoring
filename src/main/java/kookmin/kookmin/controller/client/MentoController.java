9eeb487c716f60b5956dfd97c47e202a5d4a3918
elliott0kim <jeremy8n8@gmail.com>
2024-09-18 15:07:38 +0900

    메인페이지에서 멘토페이지 연결

--- src/main/java/kookmin/kookmin/controller/client/MentoController.java
+++ src/main/java/kookmin/kookmin/controller/client/MentoController.java
@@ -28,13 +28,11 @@ public class MentoController {
     @GetMapping("/mento")
     public String mentoInfo(@RequestParam("mentoId") String id, Model model, HttpSession session) {
         //임시
-        String aid = "f9c1e528-7304-11ef-978d-f98ac74a1ba7";
-
-        MentoDto m = mentoService.findByMentoId(aid);
+        MentoDto m = mentoService.findByMentoId(id);
         model.addAttribute("mento", m);
         model.addAttribute("mentoU", userService.findByUserId(m.getUserId()));
-        model.addAttribute("mentoInfoNums", mentoService.mentoNumInfo(aid));
-        model.addAttribute("mentoActCampus", mentoService.findActByMentoId(aid));
+        model.addAttribute("mentoInfoNums", mentoService.mentoNumInfo(id));
+        model.addAttribute("mentoActCampus", mentoService.findActByMentoId(id));
         session.removeAttribute("r");
 
         return "mentoInfo";
